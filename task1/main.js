2da750acce447414e3cca9f96f1c7a8d05ec6e65
zohde <zohdetamimi07@gmail.com>
2022-11-03 16:27:47 +0200

    tasks

---
+++ task1/main.js
@@ -0,0 +1,19 @@
+let x = prompt("Please enter first value");
+let opreator = prompt('opretor pls')
+let y = prompt("Please enter your second");
+
+
+// TASK 1
+if (x > y) {
+    alert("hello world")
+  document.getElementById("demo").innerHTML =
+  "hello world";
+  console.log("hello world")
+}else{
+    alert("goodbey")
+  document.getElementById("demo").innerHTML =
+  "goodbey";
+  console.log("goodbey")
+}
+
+
--- task2/main.js
+++ task2/main.js
@@ -3,18 +3,6 @@ let opreator = prompt('opretor pls')
 let y = prompt("Please enter your second");
 
 
-// TASK 1
-// if (x > y) {
-//     alert("hello world")
-//   document.getElementById("demo").innerHTML =
-//   "hello world";
-//   console.log("hello world")
-// }else{
-//     alert("goodbey")
-//   document.getElementById("demo").innerHTML =
-//   "goodbey";
-//   console.log("goodbey")
-// }
 
 
 // TASK 2
