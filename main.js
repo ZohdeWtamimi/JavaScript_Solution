c56c1e078b116ca80c7cc72fea961d791cf62c10
zohde <zohdetamimi07@gmail.com>
2022-11-07 15:17:45 +0200

    js func

--- main.js
+++ main.js
@@ -10,41 +10,41 @@
 
 
 // A journey by Bus 
-let zohde = 0
-let weekDays = 7
-let price = .25
-do{
-    weekDays--
-    console.log('just arrived 😅');
-    console.log(`${weekDays} days lift`)
-}while(zohde >= .25); // false
+// let zohde = 0
+// let weekDays = 7
+// let price = .25
+// do{
+//     weekDays--
+//     console.log('just arrived 😅');
+//     console.log(`${weekDays} days lift`)
+// }while(zohde >= .25); // false
 
-console.log(zohde >= .25)
+// console.log(zohde >= .25)
 
-// Next Day
-let todayBudget = .50;
-// Bus Driver said:
-if(todayBudget >= .50){   
-    let yesterdayBill = .25 
-    zohde = todayBudget - yesterdayBill - price; 
-    weekDays--
-    console.log(`${weekDays} days lift`)
-}
+// // Next Day
+// let todayBudget = .50;
+// // Bus Driver said:
+// if(todayBudget >= .50){   
+//     let yesterdayBill = .25 
+//     zohde = todayBudget - yesterdayBill - price; 
+//     weekDays--
+//     console.log(`${weekDays} days lift`)
+// }
 
-console.log('debt 0')
+// console.log('debt 0')
 
-zohde = .25
-// days lift
-while(zohde >= .25 && weekDays > 0){
+// zohde = .25
+// // days lift
+// while(zohde >= .25 && weekDays > 0){
     
-    weekDays--
-    console.log(`${weekDays} days lift`)
-    if(weekDays !== 0){
-        console.log('منك لله')
-    }else{
-        console.log("thank God it's thursday")
-    }
-}
+//     weekDays--
+//     console.log(`${weekDays} days lift`)
+//     if(weekDays !== 0){
+//         console.log('منك لله')
+//     }else{
+//         console.log("thank God it's thursday")
+//     }
+// }
 
 // how treat with people
 // console.log('//////////////////')
@@ -57,4 +57,43 @@ while(zohde >= .25 && weekDays > 0){
 
 
 // warning
+
 // alert('ان كل هذه الشخصيات هي من وحي خيال المؤلف ولا تمت للواقع بأي صلة مباشرة أو غير مباشرة أو اي طرف من الاطراف و كل تشابه في الاسماء او الاشخاص هو وليد الصدفة')
+
+
+
+
+
+// PRESENTION 7/ 11
+
+// trim()
+// const greeting = '   Hello world!   ';
+// console.log(greeting);
+// console.log(greeting.trim());
+
+
+// split()
+// const str = 'The quick brown fox jumps over the lazy dog.';
+
+// const words = str.split(' ');
+// console.log(words[3]);
+// console.log(words);
+
+// const chars = str.split('');
+// console.log(chars[8]);
+// console.log(chars);
+
+// const strCopy = str.split();
+// console.log(strCopy);
+
+// STARTSWITH
+// let text = "Hello world, welcome to the universe.";
+// let result = text.startsWith("world", 6);
+// console.log(result);
+
+// REAPAT()
+// A = "hello";
+// a = A.repeat(2);
+// console.log(a);
+
+
--- task5/script.js
+++ task5/script.js
@@ -6,3 +6,5 @@ for(let i =0; i<1000; i++){
     }
 }
 console.log('count of numbers ' + counter)
+
+
